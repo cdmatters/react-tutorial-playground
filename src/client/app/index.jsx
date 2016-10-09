785ca877d88055b628cc0ef1c61d2068df3bf4b5
Eric Hambro <eric.hambro@gmail.com>
2016-10-09 16:36:12 +0100

    now using dynamic data in reusable JSX views.

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -14,16 +14,52 @@ var App = React.createClass(
       }
   });
 
-
 var JSXApp = React.createClass(
   { 
     render: function()
       {
+
+        var data = [{ "when": "2 minutes ago",
+              "who": "Jill Weeze",
+              "description": "Created new account"
+            },
+            {
+              "when": "1 hour ago",
+              "who": "Lose White",
+              "description": "Added fist chapter"
+            },
+            {
+              "when": "2 hours ago",
+              "who": "Jordan Whash",
+              "description": "Created new account"
+            }];
+            
+        var rows = data.map(
+          function(row)
+          {
+            return <tr>
+               <td>{row.when}</td>
+               <td>{row.who}</td>
+               <td>{row.description}</td>
+            </tr>
+          }
+        );
+
         return <div>
-            Hello, from Eric! It works
-            </div>;
+          <h1> Recent Changes </h1>
+          <table>
+            <thead>
+              <th>When</th>
+              <th>Who</th>
+              <th>Description</th>
+            </thead>
+            {rows}
+          </table>
+      </div>
       }
   });
+
+
 
 
 ReactDOM.render(React.createElement(App), document.body);
